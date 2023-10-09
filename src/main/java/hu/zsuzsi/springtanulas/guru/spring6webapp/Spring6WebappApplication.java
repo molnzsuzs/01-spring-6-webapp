8ef176dd366b1d81cb5c0debbc34f46c3902c49d
zsuzsi <molnzsuzs@gmail.com>
2023-10-09 22:45:49 +0200

    init

---
+++ src/main/java/hu/zsuzsi/springtanulas/guru/spring6webapp/Spring6WebappApplication.java
@@ -0,0 +1,14 @@
+package hu.zsuzsi.springtanulas.guru.spring6webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6WebappApplication {
+
+	public static void main(String[] args) {
+		// x
+		SpringApplication.run(Spring6WebappApplication.class, args);
+	}
+
+}
